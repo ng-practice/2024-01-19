4be5e8f5de190edc8789b422c83e3bd3673ef927
Gregor Woiwode <gregor.woiwode@gmail.com>
2024-01-18 21:16:12 +0100

    chore: use ws as component prefix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'nde-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "nde-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Your';
+  title = "Your";
 }
