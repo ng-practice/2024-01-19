86a309a04dc0f0d4e5f6be2bec842dc335399c19
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-01-19 11:35:15 +0100

    refactor: remove BrowserModule

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
@@ -9,5 +9,5 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule)],
+  providers: [],
 }).catch(err => console.error(err));
