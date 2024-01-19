5a205fc62af5dcfdcb66c14e4489be5e27d71e3a
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-01-19 13:15:05 +0100

    solve: use *ngFor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,27 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { TodoCheckerComponent } from './todo/todo-checker/todo-checker.component';
 
 @Component({
   selector: 'ws-root',
   standalone: true,
-  imports: [TodoCheckerComponent],
+  imports: [NgFor, TodoCheckerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  todo = { text: '🏃🏻‍♂️ Work out', isDone: true };
+  todos = [
+    { text: '🏃🏻‍♂️ Work out', isDone: true },
+    { text: '🍕 Make something to eat', isDone: false },
+  ];
 
-  notify(todo) {
-    alert(`"${todo.text}" has been clicked.`);
+  toggleTodo(todoForUpdate) {
+    const index = this.todos.findIndex(todo => todo.text === todoForUpdate.text);
+
+    if (index === -1) {
+      return;
+    }
+
+    this.todos[index].isDone = !this.todos[index].isDone;
   }
 }
